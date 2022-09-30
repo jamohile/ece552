bf2e510e21e5a96be8849d8c6bfd74722d3a5482
Jay Mohile <jamohile@gmail.com>
2022-09-29 22:21:17 -0400

    feat: add scaffold for 1st microbenchmark

---
+++ lab1/microbenchmarks/q1_stall_2cycle.c
@@ -0,0 +1,18 @@
+// The goal of this microbenchmark is to validate the presence of 2-cycle stalls.
+// We expect these to occur when one instruction uses the output of its immediate predecesor.
+// Since this processor is not bypassed/forwarded, it does not matter what the instrs are:
+// No matter what, data becomes available during writeback, and must be consumed during decode.
+
+int main(void) {
+  int a = 0;
+  int b = 0;
+
+  // Now, have to be careful here.
+  // This is easily optimized out.
+  for (int i = 0; i < 1E7; i++) {
+    // The goal here is just to make a simple write, and a simple read.
+    a = i;
+    b = a;
+  }
+  return 0;
+}
