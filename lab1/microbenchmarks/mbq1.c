81bea66c591d6cbccdcf41d6b0a80a5dda9868ad
Jay Mohile <jay.mohile@mail.utoronto.ca>
2022-10-06 00:33:03 -0400

    docs: improve microbenchmark documentation

--- lab1/microbenchmarks/mbq1.c
+++ lab1/microbenchmarks/mbq1.c
@@ -1,16 +1,27 @@
 // Number of times to repeat benchmark.
+// Note: it's important to use the raw number instead of 1eX notation.
+//       that becomes a float, with extra instructions.
 #define ITERS 10000000
 
 // A step to separate the content of the benchmark from the loop itself.
 // Otherwise, the loop operations could add their own hazards.
-#define BUFFER_LOOP() asm("nop"); asm("nop"); asm("nop")
+#define RAW_BUFFER() asm("nop"); asm("nop"); asm("nop")
 
+// If this mb runs for X iterations, we expect (ignoring overhead):
+// - 3X 2 cycle stalls.
+//   - 1X from the actual forced 2-cycle we perform.
+//   - 1X from the RAW present in the 2-cycle while-loop. (the "lt" check expands to two instrs)
+//   - 1X from the RAW present in the 1-cycle while-loop.
+// - 1X 1 cycle stalls.
+//   - only from the forced 1-cycle stall.
 int main(void) {
   int a = 0;
   int b = 0;
 
   int c = 0;
   int d = 0;
+
+  int X = ITERS;
 
   // This follows the logic presented in the 1 cycle and 2cycle files, combining them for submission.
   
@@ -19,23 +30,23 @@ int main(void) {
   
   // Force a two-cycle stall on each iteration.
   // This is caused by an immediate data access.
-  while (a < ITERS) {
+  while (a < X) {
     a += 1;
     // In the for loop case, GCC was smart enough to add the loop-increment here,
     // Which turned this into a 1-cycle stall.
     b += a;
 
-    BUFFER_LOOP();
+    RAW_BUFFER();
   }
 
   // Force a 1-cycle stall on each iteration.
   // This is caused by a data access separated by one instruction.
-  while (c < ITERS) {
+  while (c < X) {
     c += 1;
     asm("nop");
     d += c;
 
-    BUFFER_LOOP();
+    RAW_BUFFER();
   }
 
   return a + b + c + d;
