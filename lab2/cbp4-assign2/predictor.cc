ffb78d69f271a4d7531581811839f1c8bc766fc7
Jay Mohile <jay.mohile@mail.utoronto.ca>
2022-10-12 03:02:25 -0400

    feat: add logic for 2-level predictor datastruct, get, init

--- lab2/cbp4-assign2/predictor.cc
+++ lab2/cbp4-assign2/predictor.cc
@@ -45,7 +45,7 @@ bool GetPrediction_2bitsat(UINT32 PC) {
 
 void UpdatePredictor_2bitsat(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget) {
   int key = MASK_KEY_2BITSAT & PC;
-  
+
   auto currentStatus = prediction_table_2bitsat.entries[key].status;
   bool correct = resolveDir == predDir;
 
@@ -74,15 +74,54 @@ void UpdatePredictor_2bitsat(UINT32 PC, bool resolveDir, bool predDir, UINT32 br
 // 2level
 /////////////////////////////////////////////////////////////
 
+#define NUM_BHT_2LEVEL (512)
+#define BITS_HISTORY_BHT_2LEVEL (6)
+#define NUM_PHT_2LEVEL (8)
 
+// Number of unique patterns to maintain history for, per PHT.
+// This must be set as 2 ** BITS_HISTORY_LEVEL
+#define PATTERNS_2LEVEL (64)
+
+#define MASK_KEY_BHT_2LEVEL (NUM_BHT_2LEVEL - 1)
+#define MASK_KEY_PHT_2LEVEL (NUM_PHT_2LEVEL - 1)
+
+struct __attribute__((__packed__)) {
+  struct {
+    bool history[BITS_HISTORY_BHT_2LEVEL];
+  } entries[NUM_BHT_2LEVEL];
+} bhts_2level = {0};
+
+struct __attribute__((__packed__)) {
+  struct {
+    struct {
+      Counter2BitSat status : 2;
+    } patterns [PATTERNS_2LEVEL];
+  } entries[NUM_PHT_2LEVEL];
+} phts_2level;
 
 void InitPredictor_2level() {
-
+  // Initialize all pattern predictors.
+  for (int i = 0; i < NUM_PHT_2LEVEL; i++) {
+    for (int j = 0; j < PATTERNS_2LEVEL; j++) {
+      phts_2level.entries[i].patterns[j].status = WEAK_NOT_TAKEN;
+    }
+  }
 }
 
 bool GetPrediction_2level(UINT32 PC) {
+  // TODO: I don't like that this offset is hardcoded.
+  int key_bht = (PC >> 3) & MASK_KEY_BHT_2LEVEL;
+  int key_pht = PC & MASK_KEY_PHT_2LEVEL;
 
-  return TAKEN;
+  // TODO: check if this cast is OK.
+  auto history = (int*) bhts_2level.entries[key_bht].history;
+  auto status = phts_2level.entries[key_pht].patterns[*history].status;
+
+  if (status >= WEAK_TAKEN) {
+    return TAKEN;
+  }
+
+  return NOT_TAKEN;
 }
 
 void UpdatePredictor_2level(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget) {
