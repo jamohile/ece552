7e3fb0422e03672c41864a3f10c4004ec9ee0225
Jay Mohile <jay.mohile@mail.utoronto.ca>
2022-10-12 13:18:44 -0400

    refactor: predictor to use class based architecture

--- lab2/cbp4-assign2/predictor.cc
+++ lab2/cbp4-assign2/predictor.cc
@@ -16,65 +16,66 @@
 // due to CPP limits, can't be auto-generated.
 #define BITS_KEY_2BITSAT (12)
 
-enum Counter2BitSat {
-  STRONG_NOT_TAKEN = 0,
-  WEAK_NOT_TAKEN = 1,
-  WEAK_TAKEN = 2,
-  STRONG_TAKEN = 3
-};
+class Counter2BitSat {
+  enum States {
+    STRONG_NOT_TAKEN = 0,
+    WEAK_NOT_TAKEN = 1,
+    WEAK_TAKEN = 2,
+    STRONG_TAKEN = 3
+  };
 
-// Increment a 2bitsat counter with the result of a computation.
-void UpdateCounter2BitSat(Counter2BitSat* counter, bool correct) {
-  // If we are correct, then we only have to update if not already saturated.
-  // Otherwise, we must 'desaturate' or switch prediction.
-  // TODO: a more clever encoding may make this less verbose, but I'm sensing the verbosity of this is a tradeoff with the get.
-  if (correct) {
-    switch (*counter) {
-      case WEAK_NOT_TAKEN:    *counter = STRONG_NOT_TAKEN;
-        break;
-      case WEAK_TAKEN:        *counter = STRONG_TAKEN;
-        break;
-      default: break;
+  private:
+    States state;
+  
+  public:
+    Counter2BitSat(): state(WEAK_NOT_TAKEN) {}
+
+    void update(bool correct) {
+      // If we are correct, then we only have to update if not already saturated.
+      // Otherwise, we must 'desaturate' or switch prediction.
+      // TODO: a more clever encoding may make this less verbose, but I'm sensing the verbosity of this is a tradeoff with the get.
+      if (correct) {
+        switch (state) {
+          case WEAK_NOT_TAKEN:    state = STRONG_NOT_TAKEN;
+            break;
+          case WEAK_TAKEN:        state = STRONG_TAKEN;
+            break;
+          default: break;
+        }
+      } else {
+        switch (state) {
+          case STRONG_NOT_TAKEN:  state = WEAK_NOT_TAKEN;
+          case WEAK_NOT_TAKEN:    state = WEAK_TAKEN;
+          case WEAK_TAKEN:        state = WEAK_NOT_TAKEN;
+          case STRONG_TAKEN:      state = WEAK_TAKEN;
+        }
+      }
     }
-  } else {
-    switch (*counter) {
-      case STRONG_NOT_TAKEN:  *counter = WEAK_NOT_TAKEN;
-      case WEAK_NOT_TAKEN:    *counter = WEAK_TAKEN;
-      case WEAK_TAKEN:        *counter = WEAK_NOT_TAKEN;
-      case STRONG_TAKEN:      *counter = WEAK_TAKEN;
+
+    bool predict() {
+      if (state >= WEAK_TAKEN) {
+        return TAKEN;
+      }
+      return NOT_TAKEN;
     }
-  }
-}
+};
 
 struct keyed_pc_2bitsat {
   unsigned int key : BITS_KEY_2BITSAT;
 };
 
-struct {
-  struct {
-    Counter2BitSat status;
-  } entries[ENTRIES_2BITSAT];
-} prediction_table_2bitsat;
+Counter2BitSat prediction_table_2bitsat[ENTRIES_2BITSAT];
 
-void InitPredictor_2bitsat() {
-  // Initialize all prediction table entries to same value.
-  for (int i = 0; i < ENTRIES_2BITSAT; i++) {
-    prediction_table_2bitsat.entries[i].status = WEAK_NOT_TAKEN;
-  }
-}
+void InitPredictor_2bitsat() {}
 
 bool GetPrediction_2bitsat(UINT32 PC) {
   auto pc = (struct keyed_pc_2bitsat*) &PC;
- 
-  if (prediction_table_2bitsat.entries[pc->key].status >= WEAK_TAKEN) {
-    return TAKEN;
-  }
-  return NOT_TAKEN;
+  return prediction_table_2bitsat[pc->key].predict();
 }
 
 void UpdatePredictor_2bitsat(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget) {
   auto pc = (struct keyed_pc_2bitsat*) &PC;
-  UpdateCounter2BitSat(&prediction_table_2bitsat.entries[pc->key].status, resolveDir == predDir);
+  prediction_table_2bitsat[pc->key].update(resolveDir == predDir);
 }
 
 /////////////////////////////////////////////////////////////
@@ -99,52 +100,44 @@ struct keyed_pc_2level {
   unsigned int pht : BITS_KEY_PHT_2LEVEL;
 };
 
-struct {
-  struct {
+class History2Level {
+  private:
     unsigned int history : BITS_HISTORY_BHT_2LEVEL;
-  } entries[NUM_BHT_2LEVEL];
-} bhts_2level = {0};
+    
+  public:
+    History2Level(): history(0) {};
 
-struct {
-  struct {
-    struct {
-      Counter2BitSat status;
-    } patterns [NUM_PATTERNS_2LEVEL];
-  } entries[NUM_PHT_2LEVEL];
-} phts_2level;
-
-void InitPredictor_2level() {
-  // Initialize all pattern predictors.
-  for (int i = 0; i < NUM_PHT_2LEVEL; i++) {
-    for (int j = 0; j < NUM_PATTERNS_2LEVEL; j++) {
-      phts_2level.entries[i].patterns[j].status = WEAK_NOT_TAKEN;
+    auto get() {
+      return history;
     }
-  }
-}
+
+    auto update(bool result) {
+      history <<= 1;
+      history |= result;
+    }
+};
+
+// We have several BHTs, which track history for a bucket of PC addresses.
+History2Level bhts_2level[NUM_BHT_2LEVEL];
+
+// We have several PHTs, each of which contain several pattern-aware counters.
+Counter2BitSat phts_2level[NUM_PHT_2LEVEL][NUM_PATTERNS_2LEVEL];
+
+void InitPredictor_2level() {}
 
 bool GetPrediction_2level(UINT32 PC) {
   auto pc = (struct keyed_pc_2level*) &PC;
+  auto history = bhts_2level[pc->bht];
 
-  auto history = bhts_2level.entries[pc->bht].history;
-  auto status = phts_2level.entries[pc->pht].patterns[history].status;
-
-  if (status >= WEAK_TAKEN) {
-    return TAKEN;
-  }
-
-  return NOT_TAKEN;
+  return phts_2level[pc->pht][history.get()].predict();
 }
 
 void UpdatePredictor_2level(UINT32 PC, bool resolveDir, bool predDir, UINT32 branchTarget) {
   auto pc = (struct keyed_pc_2level*) &PC;
+  auto history = bhts_2level[pc->bht];
 
-  auto history = bhts_2level.entries[pc->bht].history;
-
-  // Update pattern counter.
-  UpdateCounter2BitSat(&phts_2level.entries[pc->pht].patterns[history].status, resolveDir == predDir);
-
-  // Update history.
-  bhts_2level.entries[pc->bht].history = (history << 1) | resolveDir;
+  phts_2level[pc->pht][history.get()].update(resolveDir == predDir);
+  history.update(resolveDir);
 }
 
 /////////////////////////////////////////////////////////////
