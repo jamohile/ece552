4275767bb82754293e294d6d7be3ff28b0da2425
Letian Zhang <letian.zhang@mail.utoronto.ca>
2022-10-21 02:17:49 -0400

    added comments

--- lab2/cbp4-assign2/mb.c
+++ lab2/cbp4-assign2/mb.c
@@ -9,15 +9,18 @@ int main()
     unsigned int b = 0;
 
     // This outer while branches (taken) to continue.
-    // It "not takens" only at the end.
-    // Therefore: it should contribute only one mispredict.
+    //  It "not takens" only at the end.
+    //  Therefore: it should contribute only one mispredict.
 
-    while (i < ITERS) {
+    while (i < ITERS)
+    {
         i += 1;
-        if (i % 7 == 0) {
+        //  it should mispredict 6
+        if (i % 7 == 0)
+        {
             b = 1;
         }
     }
-    
-    return i + b;
+
+    return 0;
 }
---
+++ lab2/cbp4-assign2/mb.s
@@ -0,0 +1,16 @@
+	.file	"mb.c"
+	.text
+	.section	.text.startup,"ax",@progbits
+	.p2align 4
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	xorl	%eax, %eax
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Debian 10.2.1-6) 10.2.1 20210110"
+	.section	.note.GNU-stack,"",@progbits
