111d0567e2878fa08e949281de72a6ccd84a6731
Letian Zhang <letian.zhang@mail.utoronto.ca>
2022-10-31 22:57:52 -0400

    useless commit

--- lab2/cbp4-assign2/mb.c
+++ lab2/cbp4-assign2/mb.c
@@ -1,26 +1,58 @@
 // easy micro benchmark
 #define ITERS 10000
+// generated assembly code
+/*
+	.file	"mb.c"
+	.text
+	.section	.text.startup,"ax",@progbits
+	.p2align 4
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	xorl	%edx, %edx
+	xorl	%ecx, %ecx
+	xorl	%eax, %eax
+	movl	$2, %r8d
+	movl	$1, %edi
+	.p2align 4,,10
+	.p2align 3
+.L3:
+	addl	$1, %eax
+	imull	$-1227133513, %eax, %esi
+	cmpl	$613566756, %esi
+	cmovbe	%r8d, %edx
+	cmovbe	%edi, %ecx
+	cmpl	$10000, %eax
+	jne	.L3
+	leal	10000(%rcx,%rdx), %eax
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Debian 10.2.1-6) 10.2.1 20210110"
+	.section	.note.GNU-stack,"",@progbits
 
 // Testcase 1:
 // When there is no content in the loop, 1943 mispredicts.
+*/
 int main()
 {
-    unsigned int i = 0;
-    unsigned int b = 0;
+	unsigned int i = 0;
+	unsigned int b = 0;
 
-    // This outer while branches (taken) to continue.
-    //  It "not takens" only at the end.
-    //  Therefore: it should contribute only one mispredict.
+	//  it should mispredict 6 times for the while loop below as initially all conditions are set to be weakly not taken
+	while (i < ITERS)
+	{
+		i += 1;
+		// furthur we expect the bits to change one more time for a taken condition
 
-    while (i < ITERS)
-    {
-        i += 1;
-        //  it should mispredict 6
-        if (i % 7 == 0)
-        {
-            b = 1;
-        }
-    }
+		if (i % 7 == 0)
+		{
+			b = 1;
+		}
+	}
 
-    return 0;
+	return 0;
 }
--- lab2/cbp4-assign2/mb.s
+++ lab2/cbp4-assign2/mb.s
@@ -7,7 +7,22 @@
 main:
 .LFB0:
 	.cfi_startproc
+	xorl	%edx, %edx
+	xorl	%ecx, %ecx
 	xorl	%eax, %eax
+	movl	$2, %r8d
+	movl	$1, %edi
+	.p2align 4,,10
+	.p2align 3
+.L3:
+	addl	$1, %eax
+	imull	$-1227133513, %eax, %esi
+	cmpl	$613566756, %esi
+	cmovbe	%r8d, %edx
+	cmovbe	%edi, %ecx
+	cmpl	$10000, %eax
+	jne	.L3
+	leal	10000(%rcx,%rdx), %eax
 	ret
 	.cfi_endproc
 .LFE0:
