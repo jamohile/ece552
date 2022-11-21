a30154f151e2ee2068861ff02451e558850121b4
Jay Mohile <jay.mohile@mail.utoronto.ca>
2022-11-20 22:27:55 -0500

    fix: remove redundant check from next-line prefetch

--- lab4/simplesim-3.0d-assig4/cache.c
+++ lab4/simplesim-3.0d-assig4/cache.c
@@ -509,12 +509,9 @@ md_addr_t get_PC();
 
 /* Next Line Prefetcher */
 void next_line_prefetcher(struct cache_t *cp, md_addr_t addr) {
-    md_addr_t  i = addr + cp->bsize;
-    md_addr_t new_block_address = CACHE_BADDR(cp, i);
-    if (cache_probe(cp, new_block_address) == 0) {
-// //   cache_access(cp, Read, next_block_address, NULL, cp->bsize, 0, NULL, NULL, 1)
-        cache_access(cp, Read, new_block_address, NULL, cp->bsize, 0, NULL, NULL, 1);      
-    }    
+    md_addr_t new_addr = addr + cp->bsize;
+    md_addr_t new_block_address = CACHE_BADDR(cp, new_addr);
+    cache_access(cp, Read, new_block_address, NULL, cp->bsize, 0, NULL, NULL, 1);        
 }
 
 /* Open Ended Prefetcher */
